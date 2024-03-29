0b1c0d43f3279a83aecdd1af756d4e3983f4ab42
Arvind <aruparekh@gmail.com>
2024-03-29 21:26:57 +0530

    feat: fetch data for user in their own page

--- src/routes/user.jsx
+++ src/routes/user.jsx
@@ -1,66 +1,92 @@
-import { Form } from "react-router-dom";
+import { useEffect } from "react";
+import { Form, useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore/lite";
+import { db } from "../services/firebase";
+import { useState } from "react";
 
 export default function User() {
-   const contact = {
-      first: "Your",
-      last: "Name",
-      avatar: "https://placekitten.com/g/200/200",
-      twitter: "your_handle",
-      notes: "Some notes",
-      favorite: true,
-   };
+   // const contact = {
+   //    first: "Your",
+   //    last: "Name",
+   //    avatar: "https://placekitten.com/g/200/200",
+   //    twitter: "your_handle",
+   //    notes: "Some notes",
+   //    favorite: true,
+   // };
+
+   const [contacts, setContacts] = useState([]);
+
+   const { userId } = useParams();
+
+   useEffect(() => {
+      const fetchUser = async () => {
+         const docs = await getDoc(doc(db, "users", userId));
+         console.log(docs.data());
+         setContacts((prev) => {
+            return [...prev, docs.data()];
+         });
+      };
+
+      fetchUser();
+   }, [userId]);
 
    return (
       <div id='contact'>
-         <div>
-            <img key={contact.avatar} src={contact.avatar || null} />
-         </div>
+         {contacts.map((contact, index) => {
+            return (
+               <div key={index}>
+                  <div>
+                     <img key={contact.avatar} src={contact.avatar || null} />
+                  </div>
 
-         <div>
-            <h1>
-               {contact.first || contact.last ? (
-                  <>
-                     {contact.first} {contact.last}
-                  </>
-               ) : (
-                  <i>No Name</i>
-               )}{" "}
-            </h1>
+                  <div>
+                     <h1>
+                        {contact.first || contact.last ? (
+                           <>
+                              {contact.first} {contact.last}
+                           </>
+                        ) : (
+                           <i>No Name</i>
+                        )}{" "}
+                     </h1>
 
-            {contact.twitter && (
-               <p>
-                  <a
-                     target='_blank'
-                     href={`https://twitter.com/${contact.twitter}`}
-                  >
-                     {contact.twitter}
-                  </a>
-               </p>
-            )}
+                     {contact.twitter && (
+                        <p>
+                           <a
+                              target='_blank'
+                              href={`https://twitter.com/${contact.twitter}`}
+                           >
+                              {contact.twitter}
+                           </a>
+                        </p>
+                     )}
 
-            {contact.notes && <p>{contact.notes}</p>}
+                     {contact.notes && <p>{contact.notes}</p>}
 
-            <div>
-               <Form action='edit'>
-                  <button type='submit'>Edit</button>
-               </Form>
-               <Form
-                  method='post'
-                  action='destroy'
-                  onSubmit={(event) => {
-                     if (
-                        !confirm(
-                           "Please confirm you want to delete this record."
-                        )
-                     ) {
-                        event.preventDefault();
-                     }
-                  }}
-               >
-                  <button type='submit'>Delete</button>
-               </Form>
-            </div>
-         </div>
+                     <div>
+                        <Form action='edit'>
+                           <button type='submit'>Edit</button>
+                        </Form>
+                        <Form
+                           method='post'
+                           action='destroy'
+                           onSubmit={(event) => {
+                              if (
+                                 !confirm(
+                                    "Please confirm you want to delete this record."
+                                 )
+                              ) {
+                                 event.preventDefault();
+                              }
+                           }}
+                        >
+                           <button type='submit'>Delete</button>
+                        </Form>
+                     </div>
+                  </div>
+               </div>
+            );
+         })}
       </div>
    );
 }
